8506def46584ae06b1b13863b3b6d8ed3de2e272
Guang-De Lin <fs.in.nccu@gmail.com>
2019-08-22 14:39:01 +0800

    Upload more gengar configs.

---
+++ edge-demo/gengar2-1-kaios-webglgame.js
@@ -0,0 +1,49 @@
+
+/* Copy and rename to 'config.js' to apply the setting */
+
+// TODO: use camelCase for variables as they're more common in Node.js world.
+let defaultConfigs = {
+  httpListenPort: 3000,
+
+  // in local dev env, change this path to your chrome binary
+  chrome_executue_path: '/data/chrome-headless/headless_shell',
+  chrome_launch_page: 'http://www.quakejs.com/play?set%20fs_game%20cpma&set%20mode_start%20FFA&set%20g_teamAutoJoin%201&map%20cpm1a',
+  chrome_viewport: {
+    width: 240,
+    height: 294,
+  },
+  chrome: {
+    cpuOnly: false,
+  },
+  serverRtcConfPortRange: {
+    min: 50000,
+    max: 51000,
+  },
+  app: {
+    webrtc: {
+      framesPerSecond: 18,
+      videoResizePercentage: 100/100,
+      useArrowKeysToMoveMouse: false,
+    },
+  },
+};
+
+// Parse env variables at runtime
+function parseEnvBool(envName, config, fieldName) {
+  if (process.env[envName] !== undefined) {
+    config[fieldName] = process.env[envName] === 'true';
+  }
+}
+
+function parseEnvInt(envName, config, fieldName) {
+  if (process.env[envName] !== undefined) {
+    config[fieldName] = parseInt(process.env[envName], 10);
+  }
+}
+
+parseEnvBool('GENGAR_CPU_ONLY', defaultConfigs.chrome, 'cpuOnly');
+parseEnvInt('GENGAR_SERVER_RTC_CONF_PORT_RANGE_MIN', defaultConfigs.serverRtcConfPortRange, 'min');
+parseEnvInt('GENGAR_SERVER_RTC_CONF_PORT_RANGE_MAX', defaultConfigs.serverRtcConfPortRange, 'max');
+parseEnvInt('GENGAR_HTTP_LISTEN_PORT', defaultConfigs, 'httpListenPort');
+
+module.exports = defaultConfigs;
